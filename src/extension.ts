e679e0e4da7395a6a6df849c07637c4f755b6b72
hankei6km <gh-hankei6km@outlook.jp>
2023-08-04 05:54:02 +0000

    refactor: Delete text tabs with one API call

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,25 @@
 import * as vscode from 'vscode'
 import { TabInputText, TabInputTextDiff } from 'vscode'
+import type { Tab, TabGroup } from 'vscode'
 import { getTargetTabs } from './lib/tab'
 
-function quitTextTabs(curTabGroup: vscode.TabGroup): Thenable<boolean> {
-  const tabs = getTargetTabs([TabInputText, TabInputTextDiff], curTabGroup.tabs)
-  if (tabs.length > 0) {
-    return vscode.window.tabGroups.close(tabs)
+function getTextTabs(tabGroups: readonly TabGroup[]): Tab[] {
+  const tabs: Tab[] = []
+
+  for (const tabGroup of tabGroups) {
+    // append th return of getTargetTabs to tabs
+    tabs.push(...getTargetTabs([TabInputText, TabInputTextDiff], tabGroup.tabs))
   }
-  return Promise.resolve(true) // close() に [] を渡した場合、true になったので
+
+  return tabs
 }
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     'extension.quitTextTabs',
     async () => {
-      // ロックしているわけではないので他で tab group を操作されたらたぶんコケる
-      // いちおう気休めにコピー(浅いけど)を使う
-      const all = [...vscode.window.tabGroups.all]
-      for await (const tabGroup of all) {
-        // 直列にするとちょっと遅かったので並行させる
-        new Promise(async (resolve) => {
-          resolve(await quitTextTabs(tabGroup))
-        })
-      }
+      const tabs = getTextTabs(vscode.window.tabGroups.all)
+      await vscode.window.tabGroups.close(tabs)
     }
   )
 
